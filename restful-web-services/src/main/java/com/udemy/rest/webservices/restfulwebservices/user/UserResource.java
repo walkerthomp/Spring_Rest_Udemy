53a380621d85bf88b689096a58bd5555b47a86b7
Walker Thompson <walkerthompson@gmail.com>
2023-10-17 13:57:46 -0400

    first push of current udemy projects

---
+++ restful-web-services/src/main/java/com/udemy/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,39 @@
+package com.udemy.rest.webservices.restfulwebservices.user;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+
+	private UserDaoService service;
+
+	
+	public UserResource(UserDaoService service) {
+		this.service = service;
+	}
+
+	// GET /users
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers() {
+		return service.findAll();
+	}
+
+	// GET /users
+	@GetMapping("/users/{id}")
+	public User retrieveUser(@PathVariable int id) {
+		return service.findOne(id);
+	}
+	
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user) {
+		service.save(user);
+		
+	}
+
+}
